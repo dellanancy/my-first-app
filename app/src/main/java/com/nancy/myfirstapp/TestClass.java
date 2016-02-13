9e54a51fbbbc27fb3e623a806ac9f0d785bfeb7b
Akirachix2 <Akirachix2>
2016-02-13 16:02:45 +0300

    first commit

---
+++ app/src/main/java/com/nancy/myfirstapp/TestClass.java
@@ -0,0 +1,24 @@
+package com.nancy.myfirstapp;
+
+/**
+ * Created by Akirachix2 on 06/02/2016.
+ */
+public class TestClass {
+    //Declare a variable
+
+    public static void main( String args[] ){
+        int age;
+        double shoesize;
+        char letter;
+        String FirstName;
+
+        //initialise the variable
+        age=10;
+        shoesize=5.5;
+        letter='A';
+        FirstName= "Android Developer";
+
+         System.out.print(age);
+    }
+}
+
---
+++ app/src/main/java/com/nancy/myfirstapp/space.java
@@ -0,0 +1,7 @@
+package com.nancy.myfirstapp;
+
+/**
+ * Created by Akirachix2 on 13/02/2016.
+ */
+public class space {
+}
